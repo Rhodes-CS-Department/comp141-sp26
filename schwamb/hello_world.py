090209f833aeaa9b7c5c86da60c0b6c461bdc2b0
Addison Schwamb <addison.schwamb@proton.me>
2026-01-16 10:03:16 -0600

    week 1 recitation

---
+++ schwamb/hello_world.py
@@ -0,0 +1,12 @@
+# Author: Addison Schwamb
+# Class: Prof. Schwamb's
+# Lab #: Lab 00
+# Pledge: I did this work all on my own.
+# Description: Prints my name
+
+# 1. FILL IN the required program heading above.
+# See the section on required comments on Canvas (Comment Guide).
+
+#2. Edit that program to print your name
+name = "Addison Schwamb"
+print("Hello", name)
