542ffc46060977d6c0ec628099605e4e5db2bd06
Mark Cooper <cooperm@rhodes.edu>
2026-01-20 10:56:33 -0600

    Week 2

---
+++ projects/project01/consumption.py
@@ -0,0 +1,20 @@
+# Author:
+# Class: 
+# Project: Program 1
+# Pledge:
+# Description:
+
+# Average Power Production for a Natural Gas Power Plant
+average_production_MW = 0 # my cat said so
+
+# Energy Consumption for an Average Household
+average_daily_energy_consumption_KWh = 0 # I counted the energy flowing into my house
+
+# Water Consumption for an Average Household
+average_daily_water_consumption_gallons = 0 # This is how much I drink, so it must be average.
+
+# Water Contained in the Memphis Aquifer
+gallons_per_aquifer = 0 # I watched a YouTube video about it
+
+# Households in Memphis, TN
+households = 0 # I asked my neighbor
